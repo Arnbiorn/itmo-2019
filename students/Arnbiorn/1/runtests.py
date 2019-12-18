7e9d1a96e025128782fb057389d58e7654b173ed
Arnbiorn <skrutelev988@gmail.com>
2019-12-18 20:07:28 +0300

    small fix

--- students/Arnbiorn/1/runtests.py
+++ students/Arnbiorn/1/runtests.py
@@ -27,25 +27,24 @@ def find_test(module):
     return dict(filter(lambda obj: obj[0].startswith(prefix), vars(module).items()))
 
 
+def validating_test(function):
+    try:
+        function
+    except AssertionError:
+        return 'Fail'
+    return 'Ok'
+
+
 def testing(directorys=''):
-    if len(directorys) == 0:
+    if not directorys:
         directorys = os.getcwd()
 
     for files in search(directorys, '{0}*{1}'.format(prefix, extension)):
         module = module_loading(files)
         if module is None:
             continue
-        for tests in find_test(module):
-            try:
-                tests[1]()
-                result = 'ok'
-            except AssertionError:
-                result = 'fail'
-            print('{0}-{1}-{2}'.format(
-                name,
-                test[0],
-                result))
-
-
-if __name__ == '__main__':
-    testing(input("Write directory path(or press Enter to work in this dir): "))  # noqa: E501
+        for tests in validating_test(module):
+            print('{0}-{1}'.format(
+                files,
+                validating_test(tests[0]),
+            ))
--- students/Arnbiorn/1/test_adult.py
+++ students/Arnbiorn/1/test_adult.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
- def is_adult(
+def is_adult(
     input_function=input,
-    today_function=datetime.now,
-):
+    today_function=datetime.now,):
+
     users_birthday = datetime.strptime(
         input_function('Your birstday:'),
         '%Y.%m.%d',
@@ -12,7 +12,7 @@ from datetime import datetime
     return delta.days / 365 >= 18
 
 
- def test_is_adult():
+def test_is_adult():
     assert is_adult(
         lambda _: '1905.04.01',
         lambda: datetime(year=2019, month=9, day=1),
@@ -22,5 +22,5 @@ from datetime import datetime
         lambda: datetime(year=2019, month=9, day=1),
     ) is False
 
- if __name__ == '__main__':
+if __name__ == '__main__':
     test_is_adult()
